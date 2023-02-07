16a939ab9c71d4b0e62bc778acc49e51834a3789
Cody Taylor <codemister99@yahoo.com>
2023-02-07 05:16:21 +0000

    Setup basic rocket application.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(decl_macro)]
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello Cody\r\n"
+}
+
+#[get("/goodbye")]
+fn goodbye() -> &'static str {
+    "Goodbye Cody\r\n"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, goodbye])
 }
