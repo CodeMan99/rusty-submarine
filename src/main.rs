9727dc5f3aa5136ebd8e3279d4e345a048e33ad1
Cody Taylor <codemister99@yahoo.com>
2023-02-20 15:14:59 +0000

    Add post request that requires a X-Api-Key header.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,37 @@
 extern crate rocket;
 
 use rocket::tokio::time::{sleep, Duration};
+use uuid::Uuid;
+use rocket::http::Status;
+use rocket::request::{Outcome, Request, FromRequest};
+
+#[derive(Debug, Clone, Copy)]
+struct ApiKey(Uuid);
+
+#[derive(Debug)]
+enum ApiKeyError {
+    Missing,
+    Invalid(&'static str),
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for ApiKey {
+    type Error = ApiKeyError;
+
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        match req.headers().get_one("x-api-key").map(Uuid::parse_str) {
+            None => Outcome::Failure((Status::BadRequest, ApiKeyError::Missing)),
+            Some(Ok(key)) => Outcome::Success(ApiKey(key)),
+            Some(Err(_err)) => Outcome::Failure((Status::BadRequest, ApiKeyError::Invalid("Unable to parse API-KEY")))
+        }
+    }
+}
+
+#[post("/typical-create")]
+fn typical_create(key: ApiKey) -> String {
+    println!("      >> Request Accepted with Api-Key: {:?}", key);
+    format!("Some value {}\r\n", Uuid::new_v4())
+}
 
 #[get("/delay/<seconds>")]
 async fn delay(seconds: u64) -> String {
@@ -22,5 +53,10 @@ fn goodbye() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, goodbye, delay])
+    rocket::build().mount("/", routes![
+        index,
+        goodbye,
+        delay,
+        typical_create,
+    ])
 }
