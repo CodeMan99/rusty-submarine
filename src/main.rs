ad59bed68eec793f68c0d4c52187a5ff3f4a34b6
Cody Taylor <codemister99@yahoo.com>
2023-05-05 04:13:55 +0000

    Add find-user route.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,45 @@ extern crate rocket;
 mod apikey;
 
 use apikey::ApiKey;
-use rocket::tokio::time::{sleep, Duration};
 use rocket::response::status::Accepted;
-use rocket::serde::Serialize;
 use rocket::serde::json::Json;
+use rocket::serde::Serialize;
+use rocket::tokio::time::{sleep, Duration};
+use rocket_db_pools::{sqlx, Database};
 use uuid::Uuid;
+
+#[derive(Database)]
+#[database("rusty_db")]
+struct RustyDb(sqlx::PgPool);
+
+#[derive(Debug, Serialize)]
+struct User {
+    id: i64,
+    username: String,
+    first: Option<String>,
+    last: Option<String>,
+    age: Option<i32>,
+}
+
+#[get("/find-user?<username>")]
+async fn find_user(db: &RustyDb, username: &str) -> Option<Json<User>> {
+    let maybe_user = sqlx::query_as!(
+        User,
+        "SELECT id, username, first, last, age FROM public.user WHERE username = $1;",
+        username
+    )
+    .fetch_optional(&db.0)
+    .await;
+
+    match maybe_user {
+        | Ok(Some(user)) => Some(Json(user)),
+        | Ok(None) => None,
+        | Err(err) => {
+            eprint!("{}", err);
+            None
+        },
+    }
+}
 
 #[derive(Serialize)]
 struct Document {
@@ -42,10 +76,7 @@ fn goodbye() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![
-        index,
-        goodbye,
-        delay,
-        typical_create,
-    ])
+    rocket::build()
+        .attach(RustyDb::init())
+        .mount("/", routes![index, goodbye, delay, typical_create, find_user])
 }
